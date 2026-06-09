aa259f7fdf125cd994fc904501dda80f05002498
MCgit3 <ping2mythili@gmail.com>
2026-06-09 19:39:26 +0700

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,12 @@
 #q3
+#task 1
+def update_dictionary(dct, key, value):
+ if key in dct:
+  print("Original value = ", dct[key])
+  dct[key]=value
+ else:
+  dct[key]=value
+ return(dct)
+#task 2
+print(update_dictionary({},"name", "Alice"))
+print(update_dictionary({"age": 25}, "age", 26))
