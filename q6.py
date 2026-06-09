1d5e1b79d73df917a2b8d4a4b4ea70ebf2fee026
MCgit3 <ping2mythili@gmail.com>
2026-06-09 21:04:00 +0700

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,1 +1,16 @@
 #q6
+#task 1
+def find_first_negative(lst):
+ i=0
+ while i<len(lst):
+  if lst[i] < 0:
+   return lst[i]
+  else:
+   i+=1
+   continue
+ 
+ return "No negatives"
+   
+#task 2
+print(find_first_negative([3, 5, -1, 7, -2, 8]))
+print(find_first_negative([2, 10, 7, 0]))
