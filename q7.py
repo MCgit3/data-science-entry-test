a24f4a5c3977d837afb3c53e684435174c7c84de
MCgit3 <ping2mythili@gmail.com>
2026-06-09 21:29:41 +0700

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,1 +1,12 @@
 #q7
+#task 1
+class Car:
+ def __init__(self,make,model,year):
+  self.make=make
+  self.model=model
+  self.year=year
+ def describe_car(self):
+  print(self.year, self.make, self.model)
+#task 2
+obj_car=Car("Toyota","Corolla", "2020")
+obj_car.describe_car()
