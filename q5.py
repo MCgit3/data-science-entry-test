78d32056a4b5fedc04760751bfb860092f4a42fa
MCgit3 <ping2mythili@gmail.com>
2026-06-09 20:11:52 +0700

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,1 +1,13 @@
 #q5
+#task1
+def check_divisibility(num, divisor):
+ if isinstance(num, (int, float)) and isinstance(divisor, (int, float)):
+  if(num%divisor)==0:
+   return True
+  else:
+   return False
+ else:
+  return False
+#task2
+print(check_divisibility(10,2))
+print(check_divisibility(7,3))
