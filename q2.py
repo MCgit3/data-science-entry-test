509c33b68a45faea7a36cf1c22c761252a3e024c
MCgit3 <ping2mythili@gmail.com>
2026-06-09 19:40:17 +0700

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,4 +1,5 @@
 #q2
+#task 1
 def find_and_replace(lst, find_val, replace_val):
  if isinstance(lst,list):
   for i in range(len(lst)):
@@ -7,6 +8,6 @@ def find_and_replace(lst, find_val, replace_val):
   print(lst)
  else:
   return None
-  
+#task 2
 find_and_replace([1,2,3,4,2,2],2,5)
 find_and_replace(["apple","banana","apple"],"apple","orange")
