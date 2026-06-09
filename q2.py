4c1081260b0e4461341c55ac3a3e21264c82561b
MCgit3 <ping2mythili@gmail.com>
2026-06-09 19:05:19 +0700

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,1 +1,12 @@
 #q2
+def find_and_replace(lst, find_val, replace_val):
+ if isinstance(lst,list):
+  for i in range(len(lst)):
+   if find_val == lst[i]:
+    lst[i]=replace_val
+  print(lst)
+ else:
+  return None
+  
+find_and_replace([1,2,3,4,2,2],2,5)
+find_and_replace(["apple","banana","apple"],"apple","orange")
