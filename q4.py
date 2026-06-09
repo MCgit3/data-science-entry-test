943a5feb31a8957fc9b6debdc38b5608629c9d0f
MCgit3 <ping2mythili@gmail.com>
2026-06-09 19:58:51 +0700

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,1 +1,12 @@
-#4
+#q4
+#task 1
+def string_reverse(s):
+ if isinstance(s,str):
+ #[start:end:step]
+  new_str=s[::-1]
+  return(new_str)
+ else:
+  return None
+#task 2
+print(string_reverse("Hello World"))
+print(string_reverse("Python"))
